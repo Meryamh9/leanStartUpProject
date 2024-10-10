b2bbfadb356a34397d8719a0ac15c8aa2a79ba89
hidass <hidassmeryam@gmail.com>
2024-10-10 16:31:52 +0200

    feat: add AccueilComponent

--- app/src/app/core/accueil/accueil.component.ts
+++ app/src/app/core/accueil/accueil.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss'
 })
--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+
   ]
 })
 export class SharedModule { }
